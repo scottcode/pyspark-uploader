56077d203c1b3c339e5705fe69e22fbae4b989ef
Scott Hajek <shajek@pivotal.io>
2018-08-08 07:41:12 -0400

    add setup.py installation script

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+DISTNAME = 'pyspark_uploader'
+MAINTAINER = 'Scott Hajek'
+MAINTAINER_EMAIL = 'scott.hajek@alumni.unc.edu'
+VERSION = '0.1.dev'
+
+
+if __name__ == '__main__':
+
+    setup(
+        name=DISTNAME,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        version=VERSION,
+        packages=find_packages(),
+    )
