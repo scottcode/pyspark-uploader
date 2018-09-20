d4acc40e8b5306656e72624fb8d876a14e2085c0
Scott Hajek <shajek@pivotal.io>
2018-09-20 09:49:28 -0400

    add description and bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,16 +3,21 @@
 from setuptools import setup, find_packages
 
 
+DESCRIPTION = (
+    "Enables rapid development of packages to be used via PySpark on a Spark "
+    "cluster by uploading a local Python package to the cluster."
+)
 DISTNAME = 'pyspark_uploader'
 MAINTAINER = 'Scott Hajek'
 MAINTAINER_EMAIL = 'scott.hajek@alumni.unc.edu'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 
 if __name__ == '__main__':
 
     setup(
         name=DISTNAME,
+        description=DESCRIPTION,
         author=MAINTAINER,
         author_email=MAINTAINER_EMAIL,
         maintainer=MAINTAINER,
